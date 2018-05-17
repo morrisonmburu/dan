17b5a1f0b502657feddc2549840fb5020aca5399
MorrisMburu <morrisonmburu7@gmail.com>
2018-05-17 14:50:19 +0300

    Add all changes

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+ $("#open").click(function(event){
+ 	event.preventDefault();
+ 	openNav();
+ })
+ $("#close").click(function(event){
+ 	event.preventDefault();
+  closeNav();
+ })
+}
+});
